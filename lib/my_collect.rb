10b5106d1e1e3dc184c50ba565c065bd5ca543d7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-17 02:05:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   collection_two = []
   while i < collection.length
-  yield(collection[i])
+  collection_two << yield(collection[i])
   i += 1
   end
   collection
